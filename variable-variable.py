e875af3c3e36cf110fa6da07b90d2a887b9cbdd2
Yashliftss23 <yashjainccb27@gmail.com>
2023-12-28 21:38:57 +0530

    variable-variable.py

---
+++ variable-variable.py
@@ -0,0 +1,22 @@
+# a=3
+# print(a)
+
+a= 10
+print(type(a))
+
+a= 'Yash is great'
+print(a)
+
+a='yash'
+print(type(a))
+
+
+a,b,c,d = 1,2,3,4
+print(a)
+print(b)
+print(c)
+print(d)
+
+
+age = 20
+print("my age is :",age)
